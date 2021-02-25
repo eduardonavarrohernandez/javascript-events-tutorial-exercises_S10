3bf88118da89612984f8f0fd6499b760031af2ba
eduardonavarrohernandez <enavarro1@outlook.es>
2021-02-25 04:31:32 +0000

    first commit

--- .breathecode/resets/03-sum-values/index.js
+++ .breathecode/resets/03-sum-values/index.js
@@ -1,11 +1,8 @@
-window.calculateSumListener = function calculateSumListener() {
+window.calculateSumListener = function calculateSumListener()
 	//Return the value of the input #firstNumber
 	var stringA = document.getElementById("firstNumber").value;
 	//Return the value of the input #secondNumber
 	var stringB = document.getElementById("secondNumber").value;
-	//your code goes here
-	var suma = parseInt(stringA) + parseInt(stringB);
 
-	alert(suma);
-	document.getElementById("resultNumber").innerHTML = suma;
+	//your code goes here
 };
--- exercises/01-alert-on-click/index.js
+++ exercises/01-alert-on-click/index.js
@@ -1,3 +1,17 @@
 window.myClickFunction = function myClickFunction() {
 	alert("Your first function!");
 };
+
+window.myEventListener = function myEventListener() {
+	alert("Your first function Listener!");
+};
+
+document.getElementById("myBtn").addEventListener("click", displayDate);
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+    
+
+
+    
+}
